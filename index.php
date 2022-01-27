7f9a646c963f9868d1787ceabba8ff5e039c446f
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2022-01-27 17:38:43 +0100

    Creato un method e stampato le istanze

--- index.php
+++ index.php
@@ -14,6 +14,11 @@
             $this->vote = $_vote;
         }
 
+        public function movieFormat() {
+            $formattedMovie = $this->title . ' - ' . $this->date . ' | ' . $this->vote;
+            return $formattedMovie;
+        }
+
     }
 
     $film1 = new Movie('Ritorno al Futuro', '1985', 8.5);
@@ -25,6 +30,24 @@
     $film3 = new Movie('Harry Potter e la pietra filosofale', '2001');
     var_dump($film3);
 
-
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-oop-1</title>
+</head>
+<body>
+    <main>
+        <h1>Movies</h1>
+        <ul>
+            <li><?php echo $film1->movieFormat() ?></li>
+            <li><?php echo $film2->movieFormat() ?></li>
+            <li><?php echo $film3->movieFormat() ?></li>
+        </ul>
+    </main>
+</body>
+</html>
