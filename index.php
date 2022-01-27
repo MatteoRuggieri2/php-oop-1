39697b4b003ce570644fce498d322bbeb506e35c
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2022-01-27 17:39:49 +0100

    Rimosso var_dump

--- index.php
+++ index.php
@@ -22,13 +22,10 @@
     }
 
     $film1 = new Movie('Ritorno al Futuro', '1985', 8.5);
-    var_dump($film1);
 
     $film2 = new Movie('Fast & Furious 6', '2013', 7);
-    var_dump($film2);
 
     $film3 = new Movie('Harry Potter e la pietra filosofale', '2001');
-    var_dump($film3);
 
 ?>
 
